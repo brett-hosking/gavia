73fcdd1fad8189e266c6995a770928194d7e5931
Brett Hosking <brett.hosking@gmail.com>
2019-05-31 11:09:58 +0100

    after cruise update

--- gavia/__init__.py
+++ gavia/__init__.py
@@ -2,6 +2,9 @@
 from gavia import camera
 from gavia import vizualise
 from gavia import nav
+from gavia import gps
+from gavia import log
+from gavia import time
 
 # For nested packages 
 from gavia import image 
